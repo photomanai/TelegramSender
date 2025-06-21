da902bef79291f72b2d5f7ce27d60e511c1fc5ed
photomanai <photom4n.41@gmail.com>
2025-06-21 18:00:15 +0400

    Sender

--- api/main.py
+++ api/main.py
@@ -158,5 +158,5 @@ async def send_invites():
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 5050))
     app.run(host="0.0.0.0", port=port, debug=False)
